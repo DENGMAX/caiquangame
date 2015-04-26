4c857fb57984c94c91ba15fd4f12ec6de4ade4f5
DENGMAX <444910328@QQ.COM>
2015-04-26 14:55:12 +0800

    add4
    完善程序生成APK

---
+++ src/com/guessgame/Update.java
@@ -0,0 +1,13 @@
+package com.guessgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Update extends Activity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.update);
+}
+}
